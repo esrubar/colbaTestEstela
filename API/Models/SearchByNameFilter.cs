c56685e021223520515cc4b15d17f021669ab60c
Estela <estela.rubio@colba.es>
2022-12-02 14:09:05 +0100

    Search filter

--- API/Models/SearchByNameFilter.cs
+++ API/Models/SearchByNameFilter.cs
@@ -2,5 +2,8 @@ namespace API.Models;
 
 public class SearchByNameFilter
 {
-    
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Page { get; set; }
+    public int ElementsInPageCount { get; set; }
 }
