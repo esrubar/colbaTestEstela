35946aac839932c0f3a1b8661e479db6962c425a
Estela <estela.rubio@colba.es>
2022-12-02 14:08:49 +0100

    Search Filter

--- API/Controllers/ThumbnailController.cs
+++ API/Controllers/ThumbnailController.cs
@@ -49,5 +49,56 @@ public class ThumbnailController: ControllerBase
         return NoContent();
     }
 
+    [HttpGet("search")]
+    public SearchByNamePaginationResponse SearchByName([FromQuery]SearchByNameFilter filter)
+    {
+        var elementsInPage = new List<Thumbnail>();
+    
+        var thumbnails = _thumbnails
+            .Where(t => t.Name.ToLower().Contains(filter.Name.ToLower()) 
+                        && t.Description.ToLower().Contains(filter.Description.ToLower())).ToList();
+
+        for (var i = 0; i <= filter.ElementsInPageCount - 1; i++)
+        {
+            var element = thumbnails.ElementAtOrDefault(filter.ElementsInPageCount * (filter.Page - 1) + i);
+            if (element != null) elementsInPage.Add(element);
+        }
+        
+        /* 
+        var firstElement = thumbnails.ElementAtOrDefault(2 * (filter.Page - 1));
+
+        if (firstElement != null)
+        {
+            elementsInPage.Add(firstElement);
+        }
+        
+        var secondElement = thumbnails.ElementAtOrDefault(2 * filter.Page - 1);
+        
+        if (secondElement != null)
+        {
+            elementsInPage.Add(secondElement);
+        }
+        */
+
+         
+        var total = (double)thumbnails.Count / filter.ElementsInPageCount;
+        var totalPages = (int)Math.Ceiling(total);
+        
+        /*
+         var elementsInPage = new List<Thumbnail>()
+         {
+             firstElement,
+             secondElement
+         };
+         */
+
+        var searchByNamePaginationResponse =
+            new SearchByNamePaginationResponse(filter.Page, totalPages, filter.ElementsInPageCount, elementsInPage);
+
+        return searchByNamePaginationResponse;
+            
+
+
+    }
     
 }
