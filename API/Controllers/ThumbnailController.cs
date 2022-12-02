99adedf6ec518f3d00d7822c7052650e14c34d4b
Estela <estela.rubio@colba.es>
2022-12-02 14:05:10 +0100

    Add post, delete and get by id

--- API/Controllers/ThumbnailController.cs
+++ API/Controllers/ThumbnailController.cs
@@ -16,4 +16,38 @@ public class ThumbnailController: ControllerBase
         return _thumbnails;
     }
     
+    [HttpPost]
+    public Thumbnail Create([FromBody] Thumbnail thumbnail)
+    {
+        thumbnail.Id = Guid.NewGuid();
+        _thumbnails.Add(thumbnail);
+        return thumbnail;
+    }
+     
+    [HttpGet("{id:guid}")]
+    public IActionResult GetById([FromRoute] Guid id)
+    {
+        var thumbnail = _thumbnails.FirstOrDefault(thumbnail => thumbnail.Id == id);
+        if (thumbnail is null)
+        {
+            return NotFound();
+        }
+
+        return new ObjectResult(thumbnail) { StatusCode = StatusCodes.Status200OK };
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult Delete([FromRoute] Guid id)
+    {
+        var thumbnail = _thumbnails.FirstOrDefault(thumbnail => thumbnail.Id == id);
+        if (thumbnail is null)
+        {
+            return NotFound();
+        }
+
+        _thumbnails.Remove(thumbnail);
+        return NoContent();
+    }
+
+    
 }
